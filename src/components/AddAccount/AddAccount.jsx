304fb0e70725462579e60b10c9d44b9b92220213
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-06-06 19:57:44 +0900

    기본적인 버튼 추가

--- src/components/AddAccount/AddAccount.jsx
+++ src/components/AddAccount/AddAccount.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./AddAccount.css";
 // import styled from "styled-components";
 
 // const TodoHeadBlock = styled.div`
@@ -35,14 +36,21 @@ function AddAccount() {
         <input type="checkbox" defaultValue={true} />
       </div>
       <div>
+        <button>\500</button>
+        <button>\1000</button>
+        <button>\5000</button>
+        <button>\10000</button>
+      </div>
+      <div className="money-input">
         금액:
         <input type="text" />
+        <button>초기화</button>
       </div>
       <div>
         내용:
         <input type="text" />
       </div>
-      <button>지출추가하기</button>
+      <button>가계부에 등록</button>
     </>
   );
 }
