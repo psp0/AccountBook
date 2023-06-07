8162cc424af70f446a04987a000ab6d5ceade10c
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-06-07 19:33:32 +0900

    컴포넌트 구조 및 이름변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import "./App.css";
-import AddAccount from "./components/AddAccount/AddAccount";
-import ViewAccount from "./components/ViewAccount/ViewAccount";
+import AddSection from "./components/Add/AddSection";
+import ViewSection from "./components/View/ViewSection";
 
 function App() {
   return (
     <>
       <header className="App-header">가계부</header>
       <section className="App-section">
-        <div className="calendar-container">
-          <ViewAccount />
-        </div>
-        <div className="setting-container">
-          <AddAccount />
-        </div>
+        <ViewSection />
+        <AddSection />
       </section>
       <footer className="App-footer">All right reserved</footer>
     </>
---
+++ src/components/View/ViewSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ViewSection() {
+  return (
+    <div className="calendar-container">
+      <div>달력위치</div>
+    </div>
+  );
+}
+
+export default ViewSection;
