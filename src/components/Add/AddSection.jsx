3ac219da0c5d8b4f049ac745e6ff052be2ca6eb8
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-06-09 18:46:30 +0900

    수입지출 토글 이름 수정

--- src/components/Add/AddSection.jsx
+++ src/components/Add/AddSection.jsx
@@ -1,23 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./AddSection.css";
-import ToggleButton from "./InputOuputToggle/IOToggle";
-
+import ToggleButton from "./ExpendIncomeToggle/ToggleButton";
 import DateChange from "./DateChange/DateChange";
-// import styled from "styled-components";
-
-// const TodoHeadBlock = styled.div`
-//   padding-top: 48px;
-//   padding-left: 32px;
-//   padding-right: 32px;
-//   padding-bottom: 24px;
-//   border-bottom: 1px solid #e9ecef;
-//   h1 {
-//     margin: 0;
-//     font-size: 36px;
-//     color: #343a40;
-//   }
-// `;
-// import React, { useState, useEffect } from 'react';
 
 function AddSection() {
   const [money, setMoney] = useState(0);
@@ -70,49 +54,3 @@ function AddSection() {
 }
 
 export default AddSection;
-
-// function AddSection() {
-//   const [money, setMoney] = useState(0);
-//   useEffect(() => {}, [money]);
-
-//   return (
-//     <div className="setting-container">
-//       <div className="choose-IO">
-//         수입
-//         <input type="checkbox" className="Input" />
-//         지출
-//         <input type="checkbox" className="Output" defaultValue={"checked"} />
-//       </div>
-//       <div className="money-buttons">
-//         {[500, 1000, 5000, 10000].map((e) => {
-//           return (
-//             <button
-//               value={e}
-//               onClick={(event) => {
-//                 setMoney(event.currentTarget.value);
-//               }}
-//             >
-//               \{e}
-//             </button>
-//           );
-//         })}
-//       </div>
-//       <div className="money-input">
-//         금액:
-//         <input
-//           type="text"
-//           defaultValue={money}
-//           onChange={(event) => setMoney(event.target.value)}
-//         />
-//         <button>초기화</button>
-//       </div>
-//       <div>
-//         내용:
-//         <input type="text" />
-//       </div>
-//       <button>가계부에 등록</button>
-//     </div>
-//   );
-// }
-
-// export default AddSection;
