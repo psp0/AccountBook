2eb34866ba13ca2fe00d8259f46d7f4126746313
psp0 <tkdvlf9058@g.hongik.ac.kr>
2023-06-18 13:09:01 +0900

    캘린더에 가계부 금액 추가

--- src/components/ViewContainer/ViewContainer.jsx
+++ src/components/ViewContainer/ViewContainer.jsx
@@ -2,13 +2,65 @@ import React from "react";
 import { Calendar } from "antd";
 import locale from "antd/es/date-picker/locale/ko_KR";
 import "./ViewContainer.css";
+const getListData = (value) => {
+  let dateString = `${value.year()}-${String(value.month() + 1).padStart(
+    2,
+    "0"
+  )}-${String(value.date()).padStart(2, "0")}`;
+  let arr = JSON.parse(localStorage.getItem("data")).filter(
+    (e) => e.date === dateString
+  );
+  return arr;
+};
+const getMonthData = (value) => {
+  let dateString = `${value.year()}-${String(value.month() + 1).padStart(
+    2,
+    "0"
+  )}-`;
+  let arr = JSON.parse(localStorage.getItem("data")).filter(
+    (e) => e.date.indexOf(dateString) !== -1
+  );
+  let sum = arr.reduce((total, value) => {
+    return total + value.amount;
+  }, 0);
+  return sum > 0 ? "+" + sum : sum;
+};
+
 function ViewContainer() {
+  const monthCellRender = (value) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>총 수입과 지출: {num}</section>
+      </div>
+    ) : null;
+  };
+  const dateCellRender = (value) => {
+    const listData = getListData(value);
+    return (
+      <div className="">
+        {listData.map((item) => (
+          <div key={item.date + item.order}>{item.amount}</div>
+        ))}
+      </div>
+    );
+  };
+  const cellRender = (current, info) => {
+    if (info.type === "date") return dateCellRender(current);
+    if (info.type === "month") return monthCellRender(current);
+    return info.originNode;
+  };
+
   const onPanelChange = (value, mode) => {
     console.log(value.format("YYYY-MM-DD"), mode);
   };
   return (
     <div className="calendar-container">
-      <Calendar locale={locale} onPanelChange={onPanelChange} />
+      <Calendar
+        locale={locale}
+        cellRender={cellRender}
+        onPanelChange={onPanelChange}
+      />
     </div>
   );
 }
